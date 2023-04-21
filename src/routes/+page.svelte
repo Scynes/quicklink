43f4c136aaca0f6347a494e6bbd48dc1c22654ca
Scynes <dustin.greyfield@gmail.com>
2023-04-21 18:08:34 -0400

    wip: added glass styling layout for the short link focus

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <section class="shorten-link">
     <form class="width-full grid place-center" action="#">
-        <input class="shorten-input outlineless borderless text-center" type="text">
+        <input class="shorten-input outlineless borderless text-center width-full" type="text">
     </form>
 </section>
 <section class="about">
