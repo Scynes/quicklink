4bef8c8a5179c180e7a8a8f2bde13abb35a7b8ce
Scynes <dustin.greyfield@gmail.com>
2023-04-21 18:36:10 -0400

    complete: finished about section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,34 @@
     </form>
 </section>
 <section class="about">
-    <h1>Something</h1>
+    <div class="about-card">
+        <i class="fa-solid fa-bolt fa-3x icon-ecto"></i>
+        <h1>Fast</h1>
+        <p>QuickLink delivers lightning-fast redirections, ensuring minimal wait time for users accessing shortened URLs.</p> 
+    </div>
+    <div class="about-card">
+        <i class="fa-solid fa-link fa-3x icon-ecto"></i>
+        <h1>Simple</h1>
+        <p>Our user-friendly interface makes it easy for anyone to create and manage short URLs with just a few clicks.</p>
+    </div>
+    <div class="about-card">
+        <i class="fa-solid fa-handshake-simple fa-3x icon-ecto"></i>
+        <h1>Reliable</h1>
+        <p>Built on a robust infrastructure, QuickLink ensures consistent uptime and performance for your shortened links.</p>
+    </div>
+    <div class="about-card">
+        <i class="fa-solid fa-user-shield fa-3x icon-ecto"></i>
+        <h1>Secure</h1>
+        <p>Advanced security measures protect your links from unauthorized access and keep your data safe and private.</p>
+    </div>
+    <div class="about-card">
+        <i class="fa-solid fa-chart-simple fa-3x icon-ecto"></i>
+        <h1>Statistics</h1>
+        <p>Gain valuable insights through comprehensive analytics, helping you understand user engagement and optimize your marketing strategies.</p>
+    </div>
+    <div class="about-card">
+        <i class="fa-solid fa-mobile-screen-button fa-3x icon-ecto"></i>
+        <h1>Responsive</h1>
+        <p>QuickLink's adaptive design ensures seamless access and management of your links on any device or screen size.</p>
+    </div>
 </section>
