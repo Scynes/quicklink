1e222cc2c45d1bf7fd704ef72d2d89b60129a000
Scynes <dustin.greyfield@gmail.com>
2023-05-18 11:28:27 -0400

    refactor: modularized the about cards and removed repetitive code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,54 @@
+<script>
+
+    const ABOUT_STORE = [
+        {
+            icon: 'fa-bolt',
+            title: 'Fast',
+            description: 'QuickLink delivers lightning-fast redirections, ensuring minimal wait time for users accessing shortened URLs.'
+        },
+        {
+            icon: 'fa-link',
+            title: 'Simple',
+            description: 'Our user-friendly interface makes it easy for anyone to create and manage short URLs with just a few clicks.'
+        },
+        {
+            icon: 'fa-handshake-simple',
+            title: 'Reliable',
+            description: 'Built on a robust infrastructure, QuickLink ensures consistent uptime and performance for your shortened links.'
+        },
+        {
+            icon: 'fa-user-shield',
+            title: 'Secure',
+            description: 'Advanced security measures protect your links from unauthorized access and keep your data safe and private.'
+        },
+        {
+            icon: 'fa-chart-simple',
+            title: 'Statistics',
+            description: 'Gain valuable insights through comprehensive analytics, helping you understand user engagement and optimize your marketing strategies.'
+        },
+        {
+            icon: 'fa-mobile-screen-button',
+            title: 'Responsive',
+            description: "QuickLink's adaptive design ensures seamless access and management of your links on any device or screen size."
+        }
+
+    ]
+
+</script>
+
 <section class="shorten-link">
     <form class="width-full grid place-center" action="#">
-        <input class="shorten-input outlineless borderless text-center width-full" type="text">
+        <input class="shorten-input outlineless borderless text-center width-full" type="text" placeholder="https://www.quicklink.it">
     </form>
 </section>
 <section id="about" class="about">
-    <div class="about-card">
-        <i class="fa-solid fa-bolt fa-3x icon-ecto"></i>
-        <h1>Fast</h1>
-        <p>QuickLink delivers lightning-fast redirections, ensuring minimal wait time for users accessing shortened URLs.</p> 
-    </div>
-    <div class="about-card">
-        <i class="fa-solid fa-link fa-3x icon-ecto"></i>
-        <h1>Simple</h1>
-        <p>Our user-friendly interface makes it easy for anyone to create and manage short URLs with just a few clicks.</p>
-    </div>
-    <div class="about-card">
-        <i class="fa-solid fa-handshake-simple fa-3x icon-ecto"></i>
-        <h1>Reliable</h1>
-        <p>Built on a robust infrastructure, QuickLink ensures consistent uptime and performance for your shortened links.</p>
-    </div>
-    <div class="about-card">
-        <i class="fa-solid fa-user-shield fa-3x icon-ecto"></i>
-        <h1>Secure</h1>
-        <p>Advanced security measures protect your links from unauthorized access and keep your data safe and private.</p>
-    </div>
-    <div class="about-card">
-        <i class="fa-solid fa-chart-simple fa-3x icon-ecto"></i>
-        <h1>Statistics</h1>
-        <p>Gain valuable insights through comprehensive analytics, helping you understand user engagement and optimize your marketing strategies.</p>
-    </div>
-    <div class="about-card">
-        <i class="fa-solid fa-mobile-screen-button fa-3x icon-ecto"></i>
-        <h1>Responsive</h1>
-        <p>QuickLink's adaptive design ensures seamless access and management of your links on any device or screen size.</p>
-    </div>
+
+    {#each ABOUT_STORE as aboutItem}
+        <div class="about-card">
+            <i class="{aboutItem.icon} fa-solid fa-3x icon-ecto"></i>
+            <h2>{aboutItem.title}</h2>
+            <p>{aboutItem.description}</p>
+        </div>
+    {/each}
+
 </section>
